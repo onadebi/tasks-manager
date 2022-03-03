283634923b44c158e65296d43fd7846e64fd4023
Onaefe <onaefe@onaxsys.com>
2022-03-03 07:18:24 +0100

    Addition of TypeOrmModule to App.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 @Module({
   imports: [TasksModule],
   controllers: [],
--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,13 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus,{
+    message:`You have not provided a valid Enum selection`
+  })
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
